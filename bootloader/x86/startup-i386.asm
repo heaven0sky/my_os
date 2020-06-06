52fcc6effc31b24a1037a7e9e74c7b868236dd75
heaven0sky <286076057@qq.com>
2020-06-06 20:36:50 +0800

    init

--- bootloader/x86/startup-i386.asm
+++ bootloader/x86/startup-i386.asm
@@ -9,7 +9,9 @@ startup_arch:
     or eax, 1
     mov cr0, eax
 
+    jmp gdt.kernel_code:protected_mode
 USE32
+protected_mode:
 
 
 gdtr:
@@ -20,6 +22,104 @@ gdt:
 .null equ $ -gdt
     dq 0
 
+.kernel_code equ $ - gdt
+    istruc GDTEntry
+        at GDTEntry.limitl, dw 0xFFFF
+        at GDTEntry.basel, dw 0
+        at GDTEntry.basem, db 0
+        at GDTEntry.attribute, db attrib.present | attrib.user | attrib.code | attrib.readable
+        at GDTEntry.flags__limith, db 0xFF | flags.granularity | flags.default_operand_size
+        at GDTEntry.baseh, db 0
+    iend
+
+.kernel_data equ $ - gdt
+    istruc GDTEntry
+        at GDTEntry.limitl, dw 0xFFFF
+        at GDTEntry.basel, dw 0
+        at GDTEntry.basem, db 0
+        at GDTEntry.attribute, db attrib.present | attrib.user | attrib.writable
+        at GDTEntry.flags__limith, db 0xFF | flags.granularity | flags.default_operand_size
+        at GDTEntry.baseh, db 0
+    iend
+
+.user_code equ $ - gdt
+    istruc GDTEntry
+        at GDTEntry.limitl, dw 0xFFFF
+        at GDTEntry.basel, dw 0
+        at GDTEntry.basem, db 0
+        at GDTEntry.attribute, db attrib.present | attrib.ring3 | attrib.user | attrib.code | attrib.readable
+        at GDTEntry.flags__limith, db 0xFF | flags.granularity | flags.default_operand_size
+        at GDTEntry.baseh, db 0
+    iend
+
+.user_data equ $ - gdt
+    istruc GDTEntry
+        at GDTEntry.limitl, dw 0xFFFF
+        at GDTEntry.basel, dw 0
+        at GDTEntry.basem, db 0
+        at GDTEntry.attribute, db attrib.present | attrib.ring3 | attrib.user | attrib.writable
+        at GDTEntry.flags__limith, db 0xFF | flags.granularity | flags.default_operand_size
+        at GDTEntry.baseh, db 0
+    iend
+
+.user_tls equ $ - gdt
+    istruc GDTEntry
+        at GDTEntry.limitl, dw 0xFFFF
+        at GDTEntry.basel, dw 0
+        at GDTEntry.basem, db 0
+        at GDTEntry.attribute, db attrib.present | attrib.ring3 | attrib.user | attrib.writable
+        at GDTEntry.flags__limith, db 0xFF | flags.granularity | flags.default_operand_size
+        at GDTEntry.baseh, db 0
+    iend
+
+.tss equ $ - gdt
+    istruc GDTEntry
+        at GDTEntry.limitl, dw (tss.end - tss) & 0xFFFF
+        at GDTEntry.basel, dw (tss-$$+0x7C00) & 0xFFFF
+        at GDTEntry.basem, db ((tss-$$+0x7C00) >> 16) & 0xFF
+        at GDTEntry.attribute, db attrib.present | attrib.ring3 | attrib.tssAvailabe32
+        at GDTEntry.flags__limith, db ((tss.end - tss) >> 16) & 0xF
+        at GDTEntry.baseh, db ((tss-$$+0x7C00) >> 24) & 0xFF
+    iend
+.end equ $ - gdt
+
+struc TSS
+    .prev_tss resd 1    ;The previous TSS - if we used hardware task switching this would form a linked list.
+    .esp0 resd 1        ;The stack pointer to load when we change to kernel mode.
+    .ss0 resd 1         ;The stack segment to load when we change to kernel mode.
+    .esp1 resd 1        ;everything below here is unused now..
+    .ss1 resd 1
+    .esp2 resd 1
+    .ss2 resd 1
+    .cr3 resd 1
+    .eip resd 1
+    .eflags resd 1
+    .eax resd 1
+    .ecx resd 1
+    .edx resd 1
+    .ebx resd 1
+    .esp resd 1
+    .ebp resd 1
+    .esi resd 1
+    .edi resd 1
+    .es resd 1
+    .cs resd 1
+    .ss resd 1
+    .ds resd 1
+    .fs resd 1
+    .gs resd 1
+    .ldt resd 1
+    .trap resw 1
+    .iomap_base resw 1
+endstruc
+
+tss:
+    istruc TSS
+        at TSS.esp0, dd 0x800000 - 128
+        at TSS.ss0, dd gdt.kernel_data
+        at TSS.iomap_base, dw 0xFFFF
+    iend
+.end:
 
 .end equ $ - gdt
 
