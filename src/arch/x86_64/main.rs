9df1a65e52430dd9995afe976ec861da6b5f4407
heaven0sky <286076057@qq.com>
2020-06-12 22:00:34 +0800

    init

--- src/arch/x86_64/main.rs
+++ src/arch/x86_64/main.rs
@@ -1,7 +1,7 @@
 #[naked]
 #[no_mangle]
 
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+// #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 pub unsafe extern fn kmain() {
     llvm_asm!("xchg bx, bx" : : : : "intel", "volatile");
 
